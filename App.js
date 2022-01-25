2291b969ddbb976eea09d107053a7c3c6698cf48
amriyasirli <amriyasirli62@gmail.com>
2022-01-25 20:19:41 +0700

    vector-icon added

--- App.js
+++ App.js
@@ -1,14 +1,65 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import * as React from 'react';
+import { Text, StatusBar, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { List } from 'react-native-paper';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import Logo from "./src/images/loading.svg";
+function HomeScreen() {
+  const [expanded, setExpanded] = React.useState(true);
 
-const App = () => {
+  const handlePress = () => setExpanded(!expanded);
+
   return (
-    <View>
-      <Text>test</Text>
-    </View>
-  )
+    <>
+    <Logo />
+    <Ionicons name="ios-list" size={50} color="#000" />
+    <List.Section title="Accordions">
+      <List.Accordion
+        title="Uncontrolled Accordion"
+        left={props => <List.Icon {...props} icon="folder" />}>
+        <List.Item title="First item" />
+        <List.Item title="Second item" />
+      </List.Accordion>
+
+      <List.Accordion
+        title="Controlled Accordion"
+        left={props => <List.Icon {...props} icon="folder" />}
+        expanded={expanded}
+        onPress={handlePress}>
+        <List.Item title="First item" />
+        <List.Item title="Second item" />
+      </List.Accordion>
+    </List.Section>
+    </>
+  );
 }
 
-export default App
+function SettingsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Settings!</Text>
+    </View>
+  );
+}
 
-const styles = StyleSheet.create({})
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <StatusBar
+          barStyle="dark-content"
+          // animated={true}
+          // backgroundColor="#4DD4B2"
+          translucent backgroundColor="transparent"
+        />
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Settings" component={SettingsScreen} options={{ 
+              headerShown : false
+          }}/>
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
